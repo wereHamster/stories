dd58c3a8491d084ae6f067bc5bb2c76aff00892f
iff <iff@yvesineichen.com>
2020-05-18 19:41:53 +0200

    upgrade zhif/macro

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BlurHash } from '@zhif/components'
+import { Image } from "@zhif/macro";
 
-export interface ImageProps {
-  blurHash: string,
-  metadata: { width: number; height: number },
-  img: { src: string },
-  sources: Array<{ srcSet: string; type: string }>,
+export interface ImageProps extends Image {
   span?: string;
 }
 
