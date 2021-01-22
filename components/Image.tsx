585d25880bc66a38b792cd493405ee66d0195216
Tomas Carnecky <tomc@caurea.org>
2021-01-22 15:36:11 +0000

    ...

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,33 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { BlurHash } from "@zhif/components";
-import { Image } from "@zhif/macro";
+import NextImage from 'next/image'
 
-export interface ImageProps extends Image {
-  span?: string;
-}
-
-export default ({ blurHash, metadata, img, sources, span }: ImageProps) => {
-  let sizes = "(max-width: 768px) 100vw, 768px";
-  if (span == "wp") {
-    sizes = "(max-width: 1200px) 100vw, 1200px";
-  } else if (span == "fw") {
-    sizes = "100vw";
-  }
-
+export default ({ ...props }: any) => {
   return (
-    <div className={span}>
-      <Space>
-        <BlurHash>
-          <picture data-blur-hash={blurHash}>
-            {sources.map((source, i) => (
-              <source key={i} sizes={sizes} {...source} />
-            ))}
-            <img {...img} loading="lazy" {...metadata} />
-          </picture>
-        </BlurHash>
-      </Space>
-    </div>
+    <Space>
+      <NextImage {...props} />
+    </Space>
   );
 };
 
