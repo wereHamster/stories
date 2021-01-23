5c2d5b7487d0c3d5fe84c49a0b82bb54b677dc11
Tomas Carnecky <tomc@caurea.org>
2021-01-23 22:38:27 +0000

    ...

--- src/components/Image/samples/basic.tsx
+++ src/components/Image/samples/basic.tsx
@@ -4,5 +4,5 @@ import { Image } from "..";
 type Props = Partial<React.ComponentPropsWithoutRef<typeof Image>>;
 
 export default function Sample(props: Props) {
-  return <Image src="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/DJI_0059.jpg" {...props} />;
+  return <Image src="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/DJI_0059.jpg" layout="responsive" width={400} height={320} {...props} />;
 }
