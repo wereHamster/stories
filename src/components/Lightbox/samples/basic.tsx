36bc0fef94054b842a39d1aad93bebdae40877c5
Tomas Carnecky <tomc@caurea.org>
2021-01-24 08:29:16 +0000

    ...

--- src/components/Lightbox/samples/basic.tsx
+++ src/components/Lightbox/samples/basic.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import { Lightbox } from "..";
-import { importImage } from "@zhif/macro"
 import { Image } from "@/components/Image";
 
 type Props = Partial<React.ComponentPropsWithoutRef<typeof Lightbox>>;
 
 export default function Sample(props: Props) {
-  const { metadata } = importImage("https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/DJI_0059.jpg")
-
   return (
     <div style={{ height: '600px' }}>
       <Lightbox {...props}>
