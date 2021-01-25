e21c6fcab695b0107019e1764665f74bf92f3052
Tomas Carnecky <tomc@caurea.org>
2021-01-25 16:28:08 +0000

    ...

--- content/kyrgyzstan/header.tsx
+++ content/kyrgyzstan/header.tsx
@@ -1,6 +1,12 @@
 import { Header } from "@/components/Header"
 
 export default function header() {
-    return <Header image="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/vlcsnap-2021-01-14-22h08m34s340.png"
-    title="Kyrgyzstan" subtitle="Where I was meant to be" date={new Date()} />
+  return (
+    <Header
+      image="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/vlcsnap-2021-01-14-22h08m34s340.png"
+      title="Kyrgyzstan"
+      subtitle="Where I was meant to be"
+      date={new Date()}
+    />
+  );
 }
