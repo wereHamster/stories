ddc7dee7461e3521bd4e27104ce7e56354f5544f
Tomas Carnecky <tomc@caurea.org>
2021-01-27 21:34:22 +0000

    ...

--- content/where-i-was-meant-to-be/header.tsx
+++ content/where-i-was-meant-to-be/header.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/Header"
 export default function header() {
   return (
     <Header
-      image="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/vlcsnap-2021-01-14-22h08m34s340.png"
+      image="https://storage.googleapis.com/stories.caurea.org/where-i-was-meant-to-be/vlcsnap-2021-01-14-22h08m34s340.png"
       title="Kyrgyzstan"
       subtitle="Where I was meant to be"
       date={new Date()}
