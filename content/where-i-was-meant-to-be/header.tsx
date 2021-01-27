d9de0c24f0e1a3c698e5ee5a6f89e8d4ad66b232
Tomas Carnecky <tomc@caurea.org>
2021-01-27 22:22:50 +0000

    ...

--- content/where-i-was-meant-to-be/header.tsx
+++ content/where-i-was-meant-to-be/header.tsx
@@ -1,8 +1,10 @@
 import { Header } from "@/components/Header"
+import { importImage } from "@zhif/macro"
 
 export default function header() {
   return (
     <Header
+      blurHash={importImage("https://storage.googleapis.com/stories.caurea.org/where-i-was-meant-to-be/vlcsnap-2021-01-14-22h08m34s340.png").blurHash}
       image="https://storage.googleapis.com/stories.caurea.org/where-i-was-meant-to-be/vlcsnap-2021-01-14-22h08m34s340.png"
       title="Where I was meant to be"
       date={new Date(Date.parse("2021-01-27"))}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import styled from "styled-components";
 import Image from 'next/image';
-import { Meta } from "./internal";
+import { useBlurHash } from "@/hooks/useBlurHash";
+// import { Meta } from "./internal";
 
 /**
  * The underlying DOM element which is rendered by this component.
@@ -18,8 +19,17 @@ const Root = styled.header`
 
   position: relative;
 
-  img {
+  .bg {
+    position: absolute;
     z-index: -1;
+    inset: 0;
+    pointer-events: none;
+    transition: opacity .5s ease-out .1s;
+    background-size: cover;
+  }
+
+  & > div:first-child {
+    z-index: -2;
   }
 
   .title {
@@ -52,6 +62,7 @@ const Root = styled.header`
 `;
 
 interface Props extends React.ComponentPropsWithoutRef<typeof Root> {
+  blurHash?: string
   image: string
 
   title: React.ReactNode
@@ -60,11 +71,20 @@ interface Props extends React.ComponentPropsWithoutRef<typeof Root> {
 }
 
 function Header(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof Root>>) {
-  const { children, image, title, subtitle, date, ...rest } = props;
+  const { blurHash, image, title, subtitle, date, ...rest } = props;
+
+  const [loaded, setLoaded] = React.useState(false);
+  const blurHashURL = useBlurHash(blurHash);
+  React.useEffect(() => {
+    const i = new window.Image()
+    i.addEventListener("load", () => setLoaded(true), { once: true });
+    i.src = image
+  }, [image])
 
   return (
     <Root ref={ref as any} {...rest}>
       <Image src={image} layout="fill" objectFit="cover" />
+      {blurHashURL && <div className="bg" style={{  opacity: loaded ? 0 : 1, backgroundImage: `url("${blurHashURL}")` }} />}
 
       <div style={{ gridRow: 2, textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <h1 className="title">{title}</h1>
