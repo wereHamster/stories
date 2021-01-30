cc96d52ced5196f70f3f4a6ad12e9bfc23ef9e50
Tomas Carnecky <tomc@caurea.org>
2021-01-30 16:05:16 +0000

    ...

--- src/timvir/wrapper.tsx
+++ src/timvir/wrapper.tsx
@@ -4,6 +4,15 @@ import { useRouter } from "next/router";
 import * as React from "react";
 import Head from "next/head";
 import toc from './toc'
+import { MDXProviderComponents } from "@mdx-js/react";
+import { Code } from "@timvir/blocks";
+
+const mdxComponents: MDXProviderComponents = {
+  code: function code(props: any) {
+    const [, language = "markdown"] = (props.className || "").match(/^language-(.*)$/) || [];
+    return <Code language={language}>{props.children}</Code>;
+  },
+};
 
 export default function Wrapper({ children }: { children?: React.ReactNode }) {
   return (
@@ -13,7 +22,7 @@ export default function Wrapper({ children }: { children?: React.ReactNode }) {
         <link href="https://unpkg.com/@timvir/blocks@0.1.6/styles.css" rel="stylesheet" />
       </Head>
 
-      <Page location={useRouter()} Link={Link} toc={toc}>
+      <Page location={useRouter()} Link={Link} toc={toc} mdxComponents={mdxComponents}>
         {children}
       </Page>
     </>
