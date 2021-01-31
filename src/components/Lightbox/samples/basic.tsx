214998a5c840939527dd1b97efeec75a67543b2c
Tomas Carnecky <tomc@caurea.org>
2021-01-31 10:44:07 +0000

    ...

--- src/components/Lightbox/samples/basic.tsx
+++ src/components/Lightbox/samples/basic.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Lightbox } from "..";
-import { Image } from "@/components/Image";
-import { importImage } from "../../../../image.macro";
+import NextImage from "next/image";
 
 type Props = Partial<React.ComponentPropsWithoutRef<typeof Lightbox>>;
 
@@ -9,11 +8,7 @@ export default function Sample(props: Props) {
   return (
     <div style={{ height: '600px' }}>
       <Lightbox {...props}>
-        <Image
-          image={importImage("https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/DJI_0059.jpg")}
-          layout="fill"
-          style={{width:"100%", height: "100%"}}
-        />
+        <NextImage src="https://storage.googleapis.com/caurea.org/stories/kyrgyzstan/DJI_0059.jpg" objectFit="contain" layout="fill" />    
       </Lightbox>
     </div>
   );
