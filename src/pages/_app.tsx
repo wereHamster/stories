743d4319246bbc9f4ee9210e4c05b4753e52baf0
Tomas Carnecky <tomc@caurea.org>
2021-01-31 10:57:53 +0000

    ...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { IntlProvider } from 'react-intl'
+import { css } from "@linaria/core";
 
 function App({ Component, pageProps }) {
   return (
@@ -9,3 +10,19 @@ function App({ Component, pageProps }) {
 }
 
 export default App
+
+css`
+:global() {
+  html, body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    overscroll-behavior: none;
+    font-family: 'Tuna', system-ui, sans-serif;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+}
+`;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { css } from "@linaria/core";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
@@ -34,20 +33,3 @@ export default class extends Document {
     );
   }
 }
-
-css`
-:global() {
-  html, body {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    overscroll-behavior: none;
-    font-family: 'Tuna', system-ui, sans-serif;
-    font-display: swap;
-  }
-
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-}
-`;
