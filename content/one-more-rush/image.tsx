3ac4b81a0bfe31e910e8f4f9f352ed37ea67bac5
Tomas Carnecky <tomc@caurea.org>
2021-05-18 20:50:58 +0200

    ...

--- content/one-more-rush/image.tsx
+++ content/one-more-rush/image.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div style={{ width: "100vw", height: "100vh", display: "grid" }}>
       <NextImage
-        src="https://storage.googleapis.com/stories.caurea.org/where-i-was-meant-to-be/vlcsnap-2021-01-14-22h08m34s340.png"
+        src="https://storage.googleapis.com/stories.caurea.org/one-more-rush/IMG_4340.jpeg"
         layout="fill"
         objectFit="cover"
       />
